55bff3eaeea822f049ea0d29ac9277a5b97e1042
Craig Fowler <craig@wearegiants.com>
2015-09-28 14:19:08 -0700

    stuff

--- templates/sharepage.php
+++ templates/sharepage.php
@@ -1,2 +1,2 @@
-<a href="#" class="ss-social-circle ss-facebook"></a>
-<a href="#" class="ss-social-circle ss-twitter"></a>
+<a target="blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php bloginfo( 'siteurl' ) ?><?php the_permalink();?>" class="ss-social-circle ss-facebook"></a>
+<a target="blank" href="https://twitter.com/home?status=<?php the_field('twitter_copy'); ?>" class="ss-social-circle ss-twitter"></a>
